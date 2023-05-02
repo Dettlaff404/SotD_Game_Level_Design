a6373e7f66a3f4fb66214790d1ca044a8272f9e8
Dettlaff404 <120651088+Dettlaff404@users.noreply.github.com>
2023-05-03 00:55:00 +0530

    menu screen script

---
+++ Assets/Scripts/menu_btn_script.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menu_btn_script : MonoBehaviour
+{
+    public Button continue_btn;
+    public Button load_btn;
+
+    private void Awake()
+    {
+        if (PlayerPrefs.GetInt("isSaveData") == 1)
+        {
+            continue_btn.interactable = true;
+            load_btn.interactable = true;
+        }
+        else
+        {
+            continue_btn.interactable = false;
+            load_btn.interactable = false;
+        }
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitApplication()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+    }
+
+    public void LoadGame()
+    {
+        PlayerPrefs.SetInt("LoadTheData", 1);
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(1);
+    }
+
+    
+}
